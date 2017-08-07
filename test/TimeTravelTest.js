c49ec29d0c139b5a8656abf3a08ae05cfbcc8d53
Peter_Ujvari <Peter_Ujvari@epam.com>
2017-08-07 15:35:45 +0200

    tt test

--- test/TimeTravelTest.js
+++ test/TimeTravelTest.js
@@ -2,7 +2,15 @@ import '../testHelpers/TimeTravel.js'
 
 contract("TimeTravel", function(){
   it("should travel forward correct amount of blocks", function(){
-    var blocks = 100;
-    return web3
+    var blocksToJump = 100;
+    return web3.eth.blockNumber;
+    var initialBlockNumber;
+  }).then(function(blockNumber){
+    initialBlockNumber = blockNumber;
+    blocksForward(blocksToJump);
+  }).then(function(){
+    return web3.eth.blockNumber;
+  }).then(function(blockNumber){
+    assert.equal(blockNumber, initialBlockNumber + blocksToJump, "block number wasn't equal to initial block number + blocks to jump");
   });
 });

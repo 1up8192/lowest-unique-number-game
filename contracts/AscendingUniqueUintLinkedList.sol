2a61d958ccab3705d5cb45d909ffa9c7ba047c46
Peter <1up8192@gmail.com>
2017-08-24 17:15:20 +0100

    datasctructure for uncovered numbers inited

---
+++ contracts/AscendingUniqueUintLinkedList.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.11;
+
+contract AscendingUniqueUintLinkedList{
+    struct Element {
+        uint next;
+    }
+
+    uint size;
+    uint head;
+    mapping(uint => Element) elements;
+
+    function deleteElement() returns (bool){
+        return false;
+    }
+
+    function insertElement(uint n) returns (bool){
+        return false;
+    }
+}

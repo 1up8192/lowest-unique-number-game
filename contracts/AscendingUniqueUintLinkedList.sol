c132080d2ed67494ef3b60a925d8edf7fab8ab5f
Peter <1up8192@gmail.com>
2017-08-25 14:05:14 +0100

    AUULL incomplete untested

--- contracts/AscendingUniqueUintLinkedList.sol
+++ contracts/AscendingUniqueUintLinkedList.sol
@@ -2,12 +2,13 @@ pragma solidity ^0.4.11;
 
 contract AscendingUniqueUintLinkedList{
     struct Element {
-        uint next;
         uint previous;
+        uint next;
     }
 
     uint size;
     uint head;
+    uint tail;
     mapping(uint => Element) elements;
 
     function deleteElement() returns (bool){
@@ -15,6 +16,56 @@ contract AscendingUniqueUintLinkedList{
     }
 
     function insertElement(uint n) returns (bool){
+        if (n < head || head == 0){
+            elements[n] = Element({previous: 0, next: head});
+            head = n;
+        } else if (n > tail){
+            elements[n] = Element({previous: tail, next: 0});
+            tail = n;
+        } else {
+            //TODO, choose strategy and find previous element
+        }
         return false;
+    }
+
+    function insertAfterPrevious(uint n, uint previous) internal {
+        uint next = elements[previous].next;
+        elements[n].previous = previous;
+        elements[n].next = next;
+        elements[previous].next = n;
+        elements[next].previous = n;
+    }
+
+    function seekPositionSides(uint n) returns (uint) {
+        uint previous;
+        uint position;
+        if (n < (head+tail)/2){ //forward search
+            for(position = head; position < n; position = elements[position].next){
+                previous = elements[previous].previous;
+            }
+        } else { //backward search
+            for(position = tail; position > n; position = elements[position].previous){
+                previous = position;
+            }
+        }
+        return previous;
+    }
+
+    function seekPositionCenter(uint n) returns (uint) {
+        bool found = false;
+        uint distance = 1;
+        uint previous;
+        do{
+            if(elements[n-distance].next != 0){
+                previous = n-distance;
+                found = true;
+            }
+            if(elements[n+distance].previous != 0){
+                previous = elements[n+distance].previous;
+                found = true;
+            }
+            distance += 1;
+        } while(!found);
+        return previous;
     }
 }
